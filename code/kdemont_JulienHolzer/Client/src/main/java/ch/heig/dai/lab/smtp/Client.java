badf8610346345805698890bb571c8c843faa82d
kdemont <kilian.demont@heig-vd.ch>
2023-11-16 12:15:02 +0100

    ajout json

--- code/kdemont_JulienHolzer/Client/src/main/java/ch/heig/dai/lab/smtp/Client.java
+++ code/kdemont_JulienHolzer/Client/src/main/java/ch/heig/dai/lab/smtp/Client.java
@@ -5,8 +5,7 @@ import org.json.JSONObject;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static java.nio.charset.StandardCharsets.*;
 
@@ -15,30 +14,47 @@ public class Client {
     private static final int SMTP_PORT = 1025;
 
     public static void main(String[] args) {
+        // Read the number of groups provided as a command line argument
+        int n = 10; //Integer.parseInt(args[0]); //décommenter à la fin
+
         // Read victim and message lists from JSON files
-        List<String> victims = readFromJsonFile("victims.json", "emails");
-        List<String> messages = readFromJsonFile("messages.json", "messages");
+        List<String> victims = readFromJsonFile("code/kdemont_JulienHolzer/Client/src/main/resources/victims.json", "victims");
+        List<String> messages = readFromJsonFile("code/kdemont_JulienHolzer/Client/src/main/resources/messages.json", "messages");
+        //changer la logique des messages en deux array ou autre structure
 
-        // Other client logic...
-        for (String victim : victims) {
-            // Form groups, select messages, and send emails
-            // (You'll need to implement the logic for this)
-            // ...
 
-            // Example: Send email content
-            sendEmail("Your Subject", "Hello, this is the body of the email.", victim);
+        // Shuffle the list of victims
+        Collections.shuffle(victims);
+
+        // Divide the victims into n groups, each having 2-5 email addresses
+        List<List<String>> groups = divideIntoGroups(victims, n);
+
+        for (List<String> group : groups) {
+            // The first address of the group is the sender
+            String sender = group.get(0);
+
+            // The rest are receivers (victims)
+            List<String> receivers = group.subList(1, group.size());
+
+            // Select a random message from the messages list
+            String[] subjectAndMessage = getRandomSubjectAndMessage(messages);
+
+            // Send emails to the group
+            for (String receiver : receivers) {
+                sendEmail(sender, receiver, subjectAndMessage[0], subjectAndMessage[1]);
+            }
         }
     }
 
-    private static void sendEmail(String subject, String body, String recipient) {
+    private static void sendEmail(String sender, String receiver, String subject, String body) {
         try (Socket socket = new Socket(SMTP_SERVER, SMTP_PORT);
              BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), UTF_8));
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), UTF_8))) {
 
             // SMTP communication logic...
 
-            sendCommand(writer, "MAIL FROM: sender@example.com");
-            sendCommand(writer, "RCPT TO: " + recipient);
+            sendCommand(writer, "MAIL FROM: " + sender);
+            sendCommand(writer, "RCPT TO: " + receiver);
             sendCommand(writer, "DATA");
             sendCommand(writer, "Subject: " + subject);
             sendCommand(writer, ""); // Empty line before the body
@@ -81,4 +97,38 @@ public class Client {
         writer.write(command + "\n");
         writer.flush();
     }
+
+    private static List<List<String>> divideIntoGroups(List<String> list, int n) {
+        // Shuffle the list to randomize the order
+        Collections.shuffle(list);
+
+        // Divide the list into n groups
+        int groupSize = Math.min(5, Math.max(2, list.size() / n));
+        return partitionList(list, groupSize);
+    }
+
+    private static <T> List<List<T>> partitionList(List<T> list, int partitionSize) {
+        int size = list.size();
+        int partitions = (int) Math.ceil((double) size / partitionSize);
+
+        List<List<T>> partitionedList = new ArrayList<>(partitions);
+
+        for (int i = 0; i < size; i += partitionSize) {
+            int end = Math.min(size, i + partitionSize);
+            partitionedList.add(new ArrayList<>(list.subList(i, end)));
+        }
+
+        return partitionedList;
+    }
+
+    private static String[] getRandomSubjectAndMessage(List<String> messages) {
+        Random random = new Random();
+        int index = random.nextInt(messages.size());
+        if (messages.size() % 2 == 1) {
+            index--;
+        }
+
+        return new String[]{messages.get(index), messages.get(++index)};
+    }
+
 }
---
+++ code/kdemont_JulienHolzer/JsonGenerator/src/main/java/ch/heig/dai/lab/smtp/JsonGenerator.java
@@ -0,0 +1,64 @@
+package ch.heig.dai.lab.smtp;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class JsonGenerator {
+    public static void main(String[] args) {
+        generateAndSaveVictims();
+        generateAndSaveMessages();
+    }
+
+    private static void generateAndSaveVictims() {
+        // Create a JSON array for the victims list
+        JSONArray victimsArray = new JSONArray();
+
+        // Generate more than 50 victim emails
+        for (int i = 1; i <= 50; i++) {
+            victimsArray.put("victim" + i + "@example.com");
+        }
+
+        // Create a JSON object for the victims data
+        JSONObject victimsObject = new JSONObject();
+        victimsObject.put("victims", victimsArray);
+
+        // Convert the JSON object to a string
+        String jsonString = victimsObject.toString();
+
+        // Save the JSON string to a file
+        saveJsonToFile(jsonString, "code/kdemont_JulienHolzer/Client/src/main/resources/victims.json");
+    }
+
+    private static void generateAndSaveMessages() {
+        // Create a JSON array for the messages list
+        JSONArray messagesArray = new JSONArray();
+
+        // Generate more than 50 messages
+        for (int i = 1; i <= 50; i++) {
+            messagesArray.put("Subject " + i);
+            messagesArray.put("Message " + i);
+        }
+
+        // Create a JSON object for the messages data
+        JSONObject messagesObject = new JSONObject();
+        messagesObject.put("messages", messagesArray);
+
+        // Convert the JSON object to a string
+        String jsonMessagesString = messagesObject.toString();
+
+        // Save the JSON string to a file
+        saveJsonToFile(jsonMessagesString, "code/kdemont_JulienHolzer/Client/src/main/resources/messages.json");
+    }
+
+    private static void saveJsonToFile(String json, String fileName) {
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
+            fileWriter.write(json);
+            System.out.println("JSON data saved to " + fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
